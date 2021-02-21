64d11d5ec3909834d4d95f0d982266025c6e5ab2
Данила Понукаев <danila_ponukaev@mail.ru>
2021-02-21 22:27:48 +0300

    Добавлен алгоритм поиска, но вглубь он не работает, дальше стартовой парки не ухоидит. Из - за отсутсвия полного имени директорий

--- FileSearch/Seeker.cs
+++ FileSearch/Seeker.cs
@@ -17,11 +17,14 @@ namespace FileSearch
         private uint _allCountFound = 0;
         private Regex _format = new Regex(string.Empty);
         private DirectoryNode _startDirectory;
+        private DirectoryNode _currDirectory;
+        private Stack<DirectoryNode> _nodes;
 
         public uint Found { get => _countFound; }
         public uint AllFound { get => _allCountFound; }
         public string Format { set => _format = new Regex(Regex.Escape(value)); }
         public string StartDirectory { set => _startDirectory = new DirectoryNode { NodeName = value }; }
+        public string CurrentDirectory { get => _currDirectory.NodeName; }
 
         public Seeker() { }
 
@@ -35,23 +38,47 @@ namespace FileSearch
 
         private void Restart()
         {
+            _nodes = new Stack<DirectoryNode>();
+            _currDirectory = _startDirectory;
             _countFound = 0;
             _allCountFound = 0;
         }
 
         private void Seeking()
         {
+            _nodes.Push(_startDirectory);
 
+            while (CheckStop())
+            {
+                _currDirectory = _nodes.Pop();
+                CheckDirectory();
+                CheckFiles();
+            }
         }
 
         private void CheckFiles()
         {
-
+            var currDirInfo = new DirectoryInfo(_currDirectory.NodeName);
+            foreach (var file in currDirInfo.GetFiles())
+            {
+                _allCountFound++;
+                if (_format.IsMatch(file.Name))
+                {
+                    _countFound++;
+                    _currDirectory.Items.Add(new FileNode { NodeName = file.Name });
+                }    
+            }
         }
 
         private void CheckDirectory()
         {
-            
+            var currDirInfo = new DirectoryInfo(_currDirectory.NodeName);
+            foreach (var dir in currDirInfo.GetDirectories())
+            {
+                _currDirectory.Items.Add(new DirectoryNode { NodeName = dir.Name });
+            }
         }
+
+        private bool CheckStop() => _nodes.Count > 0;
     }
 }
