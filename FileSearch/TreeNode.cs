1c25a00ef39950a615fe49fbcb650645e1512f3c
Данила Понукаев <danila_ponukaev@mail.ru>
2021-02-21 20:43:34 +0300

    Добавьте файлы проекта.

---
+++ FileSearch/TreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearch
+{
+    public class TreeNode : NotifyPropertyChanged
+    {
+        private string _nodeName;
+        private ObservableCollection<TreeNode> _items;
+
+        public string NodeName
+        {
+            get => _nodeName;
+            set
+            {
+                _nodeName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<TreeNode> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
